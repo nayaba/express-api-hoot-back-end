6574f8bf2cc543bcb1d77844e7e42f08470cece2
Nabila Ayaba <nabilaayaba@gmail.com>
2024-08-14 11:29:02 +0300

    Create a PUT route to update a specific hoot blog post in the database by id

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -41,4 +41,49 @@ router.get('/:hootId', async (req, res) => {
   }
 })
 
+router.put('/:hootId', async (req, res) => {
+    try {
+
+        // find the hoot by it's id
+        const hoot = await Hoot.findById(req.params.hootId)
+        
+        // check if the author of the hoot is also the signed in user
+        if (!hoot.author.equals(req.user._id)) {
+            return res.status(403).send("You're not allowed to do that!")
+        }
+        
+        // update the hoot
+        const updatedHoot = await Hoot.findByIdAndUpdate(
+            req.params.hootId,
+            req.body,
+            { new: true }
+        )
+        
+        res.status(200).json(updatedHoot)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+router.delete('/:hootId', async (req, res) => {
+    try {
+
+        //  find the hoot we need to delete
+
+        // check if the logged in user is the author
+
+        //  delete the hoot
+
+        // respond with a status and the deleted hoot 
+
+
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
 module.exports = router
+
+
+// banana = 66bc68bc56b6bdfd13b2842c
+// hootId = 66bc68ed56b6bdfd13b2842e
