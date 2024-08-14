db3492cb1640701e562078a67d73016c85f2b333
Nabila Ayaba <nabilaayaba@gmail.com>
2024-08-14 10:46:46 +0300

    Create a GET route to retrieve a specific hoot blog post from the database by id

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -4,35 +4,41 @@ const verifyToken = require('../middleware/verify-token')
 
 const Hoot = require('../models/hoot')
 
-
 // ========= Public Routes ==============
-
 
 // ======== Protected Routes ============
 router.use(verifyToken)
 
 router.post('/', async (req, res) => {
-    try {
-        req.body.author = req.user._id
-        const hoot = await Hoot.create(req.body)
+  try {
+    req.body.author = req.user._id
+    const hoot = await Hoot.create(req.body)
 
-        res.status(201).json(hoot)
-    } catch (error) {
-        res.status(500).json(error)
-    }
+    res.status(201).json(hoot)
+  } catch (error) {
+    res.status(500).json(error)
+  }
 })
 
 router.get('/', async (req, res) => {
-    try {
-        const hoots = await Hoot.find({})
-            .populate('author')
-            .sort({ createdAt: 'desc' })
+  try {
+    const hoots = await Hoot.find({})
+      .populate('author')
+      .sort({ createdAt: 'desc' })
 
-        res.status(200).json(hoots)
-    } catch (error) {
-        res.status(500).json(error)
-    }
+    res.status(200).json(hoots)
+  } catch (error) {
+    res.status(500).json(error)
+  }
 })
 
+router.get('/:hootId', async (req, res) => {
+  try {
+    const hoot = await Hoot.findById(req.params.hootId).populate('author')
+    res.status(200).json(hoot)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
 
 module.exports = router
